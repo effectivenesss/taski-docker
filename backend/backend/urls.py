ea58cb54fe0310a903962a05c78d5a4f5140c553
Картер <effectcat@yandex.ru>
2024-03-14 06:46:24 +0500

    i

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from api import views
+from .api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
